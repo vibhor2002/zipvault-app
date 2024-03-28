06cad81d5ca44cf3575cf748d601c034d2ca8991
Vibhor Grover <grovervibhor21@gmail.com>
2024-03-28 14:18:18 +0530

    Add file upload functionality to Dropzone component Part 1

--- components/Dropzone.tsx
+++ components/Dropzone.tsx
@@ -2,10 +2,45 @@
 import { cn } from '@/lib/utils'
 import React from 'react'
 import DropzoneComp from 'react-dropzone'
+import { useState } from 'react'
+import { useUser } from '@clerk/nextjs'
+
 function Dropzone() {
+    const [loading, setLoading] = useState(false);
+    const { isLoaded, isSignedIn, user } = useUser();
+
+    const onDrop = (acceptedFiles: File[]) => {
+        acceptedFiles.forEach((file) => {
+            const reader = new FileReader();
+
+            reader.onabort = () => console.log('file reading was aborted');
+            reader.onerror = () => console.log('file reading has failed');
+
+            reader.onload = async () => {
+                await uploadPost(file);
+            };
+            reader.readAsArrayBuffer(file);
+        });
+    };
+
+    const uploadPost = async (selectedFile: File) => {
+        if (loading) return;
+        if (!user) return;
+
+        setLoading(true);
+
+        setLoading(false);
+    };
+
     const maxSize = 20971520; // 20MB
     return (
-        <DropzoneComp minSize={0} maxSize={maxSize} onDrop={acceptedFiles => console.log(acceptedFiles)}>
+
+
+        <DropzoneComp
+            minSize={0}
+            maxSize={maxSize}
+            onDrop={acceptedFiles => console.log(acceptedFiles)} >
+
             {({
                 getRootProps,
                 getInputProps,
@@ -13,7 +48,9 @@ function Dropzone() {
                 isDragReject,
                 fileRejections,
             }) => {
+
                 const isFileTooLarge = fileRejections.length > 0 && fileRejections[0].file.size > maxSize;
+
                 return (
                     <section className='m-4'>
                         <div {...getRootProps()} className={cn("w-full h-52 flex justify-center items-center p-5 border border-dashed text-center rounded-lg",
