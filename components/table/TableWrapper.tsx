3eb684a3bd54f51b0029865688956f93364b73ff
Vibhor Grover <grovervibhor21@gmail.com>
2024-04-04 15:24:04 +0530

    Add React Firebase Hooks and Sort Functionaloty Done

--- components/table/TableWrapper.tsx
+++ components/table/TableWrapper.tsx
@@ -5,7 +5,11 @@ import { Button } from '../ui/button'
 import { DataTable } from './Table'
 import { columns } from './columns'
 import { useUser } from '@clerk/nextjs'
-import { useState } from 'react'
+import { use, useEffect, useState } from 'react'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import { collection, orderBy, query } from 'firebase/firestore'
+import { db } from '@/firebase'
+import { set } from 'firebase/database'
 
 function TableWrapper(
     { skeletonFiles }: { skeletonFiles: FileType[] }
@@ -15,6 +19,32 @@ function TableWrapper(
     const [initialFiles, setInitialFiles] = useState<FileType[]>([]);
     const [sort, setSort] = useState<"asc" | "desc">('desc');
 
+    // React firebase Hooks
+    const [docs, loading, error] = useCollection(
+        user &&
+        query(
+            collection(db, "users", user.id, "files"),
+            orderBy("timestamp", sort)
+        )
+    );
+
+    useEffect(() => {
+        if (!docs) return;
+
+        const files: FileType[] = docs.docs.map((doc) => ({
+            id: doc.id,
+            filename: doc.data().filename || doc.id,
+            timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+            fullname: doc.data().fullname,
+            downloadURL: doc.data().downloadURL,
+            type: doc.data().type,
+            size: doc.data().size,
+        }));
+
+        setInitialFiles(files);
+    }, [docs]);
+
+
     return (
         <div>
             <Button onClick={() => {
@@ -23,7 +53,7 @@ function TableWrapper(
                 Sort by {sort === "desc" ? "Newest" : "Oldest"}
             </Button>
 
-            <DataTable columns={columns} data={skeletonFiles} />
+            <DataTable columns={columns} data={initialFiles} />
         </div>
     )
 }
