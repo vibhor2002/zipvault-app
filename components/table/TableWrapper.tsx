65efc07eacbd02c0d858ac30eb510e654bf915e8
Vibhor Grover <grovervibhor21@gmail.com>
2024-04-05 16:56:42 +0530

    Add skeleton loading for table component

--- components/table/TableWrapper.tsx
+++ components/table/TableWrapper.tsx
@@ -9,7 +9,7 @@ import { use, useEffect, useState } from 'react'
 import { useCollection } from 'react-firebase-hooks/firestore'
 import { collection, orderBy, query } from 'firebase/firestore'
 import { db } from '@/firebase'
-import { set } from 'firebase/database'
+import { Skeleton } from "@/components/ui/skeleton"
 
 function TableWrapper(
     { skeletonFiles }: { skeletonFiles: FileType[] }
@@ -44,13 +44,46 @@ function TableWrapper(
         setInitialFiles(files);
     }, [docs]);
 
+    if (docs?.docs.length === undefined)
+        return (
+            <div className='flex flex-col' >
+                <Button variant={'outline'} className='ml-auto mb-5 w-36 h-10'>
+                    <Skeleton className='h-5 w-full' />
+                </Button>
+
+                <div className="border rounded-lg">
+                    <div className="border-b h-12" />
+                    {skeletonFiles.map((file) => (
+                        <div
+                            key={file.id}
+                            className='flex items-center space-x-4 p-5 w-full'
+                        >
+                            <Skeleton className='h-12 w-12' />
+                            <Skeleton className='h-12 w-full' />
+                        </div>
+                    ))}
+
+                    {skeletonFiles.length === 0 && (
+                        <div className='flex items-center space-x-4 p-5 w-full'>
+                            <Skeleton className='h-12 w-12' />
+                            <Skeleton className='h-12 w-full' />
+                        </div>
+                    )}
+                </div>
+            </div>
+        );
+
 
     return (
-        <div>
-            <Button onClick={() => {
-                setSort(sort === 'desc' ? 'asc' : 'desc');
-            }}>
-                Sort by {sort === "desc" ? "Newest" : "Oldest"}
+        <div className='flex flex-col space-y-5 pb-10'>
+            <Button
+                variant={"outline"}
+                onClick={() => {
+                    setSort(sort === 'desc' ? 'asc' : 'desc');
+                }}
+                className='ml-auto w-fit'
+            >
+                Sort By {sort === "desc" ? "Newest" : "Oldest"}
             </Button>
 
             <DataTable columns={columns} data={initialFiles} />
